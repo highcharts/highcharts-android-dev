7a895a17d90b2698293916736b1ae0d5083d0e02
Bartosz Gnacek <b.gnacek@gmail.com>
2017-12-28 11:39:34 +0100

    added JavaDoc comments to HIGradient, HIStop; changed types, added exception in case of wrong range

--- highcharts-android/highcharts/src/main/java/com/highsoft/highcharts/Common/HIGradient.java
+++ highcharts-android/highcharts/src/main/java/com/highsoft/highcharts/Common/HIGradient.java
@@ -3,18 +3,30 @@ package com.highsoft.highcharts.Common;
 import java.util.HashMap;
 import java.util.Map;
 
+/**Definition of the gradient, similar to SVG: object literal holds start position (x1, y1) and the end position (x2, y2) relative to the shape, where 0 means top/left and 1 is bottom/right. All positions are floats between 0 and 1.**/
 public class HIGradient {
 
-    public String type;
-//    public int x;
-//    public int y;
-//    public int z;
-    private Number x1;
-    private Number x2;
-    private Number y1;
-    private Number y2;
+    /** Defaults to 0. **/
+    private float x1;
+    /** Defaults to 0. **/
+    private float x2;
+    /** Defaults to 0. **/
+    private float y1;
+    /** Defaults to 1. **/
+    private float y2;
 
-    public HIGradient(Number x1, Number y1, Number x2, Number y2){
+    /** Default constructor. Sets positions values to default ones. **/
+    public HIGradient(){
+        this.x1 = 0;
+        this.x2 = 0;
+        this.y1 = 0;
+        this.y2 = 1;
+    }
+
+    /** Sets positions to the desired values. Values must be in 0 - 1 range. **/
+    public HIGradient(float x1, float y1, float x2, float y2){
+        if(x1 < 0.0 || x1 > 1.0 || x2 < 0.0 || x2 > 1.0 || y1 < 0.0 || y1 > 1.0 || y2 < 0.0 || y2 > 1.0)
+            throw new IllegalArgumentException("Position of the gradient must be in 0 - 1 range");
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
@@ -23,6 +35,7 @@ public class HIGradient {
 
     private Map<String, Number> gradient = new HashMap<>();
 
+    /** Returns map representation of the gradient **/
     public Map getGradient(){
 
         gradient.put("x1", x1);
--- highcharts-android/highcharts/src/main/java/com/highsoft/highcharts/Common/HIStop.java
+++ highcharts-android/highcharts/src/main/java/com/highsoft/highcharts/Common/HIStop.java
@@ -3,8 +3,9 @@ package com.highsoft.highcharts.Common;
 import java.util.LinkedList;
 import java.util.Map;
 
+/** The stops is an LinkedList of position and HIColor, position is a float between 0 and 1 assigning the relative position in the gradient, HIColor is the desired color. **/
 public class HIStop extends LinkedList<Object>{
-
+    
     public HIStop(double position, HIColor color) {
         if(position < 0 || position > 1){
             throw new IllegalArgumentException("Position of the gradient must be in 0 - 1 range");
