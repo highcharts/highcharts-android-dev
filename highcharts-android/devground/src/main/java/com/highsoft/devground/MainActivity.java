e85b7437f379acc79af62655747a964c85607c71
bartosz <b.gnacek@gmail.com>
2018-06-02 16:05:57 +0200

    HighFit and Readme update

--- highcharts-android/devground/src/main/java/com/highsoft/devground/MainActivity.java
+++ highcharts-android/devground/src/main/java/com/highsoft/devground/MainActivity.java
@@ -3,9 +3,15 @@ package com.highsoft.devground;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.highsoft.highcharts.Common.HIChartsClasses.HIChart;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIColumn;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIDataLabels;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIOptions;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIPlotOptions;
+import com.highsoft.highcharts.Common.HIChartsClasses.HISeries;
+import com.highsoft.highcharts.Common.HIChartsClasses.HISubtitle;
 import com.highsoft.highcharts.Common.HIChartsClasses.HITitle;
+import com.highsoft.highcharts.Common.HIChartsClasses.HITooltip;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIXAxis;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIXrange;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIYAxis;
@@ -16,7 +22,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
-
 public class MainActivity extends AppCompatActivity {
 
 
@@ -26,65 +31,88 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         HIChartView chartView = findViewById(R.id.hc);
-        chartView.plugins = new ArrayList<>(Arrays.asList("xrange"));
 
         HIOptions options = new HIOptions();
 
+        HIChart chart = new HIChart();
+        chart.setType("column");
+        options.setChart(chart);
+
         HITitle title = new HITitle();
-        title.setText("Highcharts X-range");
+        title.setText("UEFA Champions League 2016/17");
+        HISubtitle subtitle = new HISubtitle();
+        subtitle.setText("Team statistics");
         options.setTitle(title);
+        options.setSubtitle(subtitle);
 
-        HIXAxis xAxis = new HIXAxis();
-        xAxis.setType("datetime");
-        options.setXAxis(new ArrayList<HIXAxis>(){{add(xAxis);}});
+        final HIXAxis hixAxis = new HIXAxis();
+        ArrayList<String> categories = new ArrayList<>();
+        categories.add("Goals");
+        categories.add("Assists");
+        categories.add("Shots On Goal");
+        categories.add("Shots");
+        hixAxis.setCategories(categories);
+        options.setXAxis(new ArrayList<HIXAxis>(){{add(hixAxis);}});
 
-        HIYAxis yAxis = new HIYAxis();
-        String[] categoriesList = new String[] {"Prototyping", "Development", "Testing" };
-        yAxis.setCategories(new ArrayList<>(Arrays.asList(categoriesList)));
-        yAxis.setTitle(new HITitle());
-        yAxis.getTitle().setText("");
-        yAxis.setReversed(true);
-        options.setYAxis(new ArrayList<HIYAxis>(){{add(yAxis);}});
+        final HIYAxis hiyAxis = new HIYAxis();
+        hiyAxis.setMin(0);
+        hiyAxis.setTitle(new HITitle());
+        hiyAxis.getTitle().setText("Number");
+        options.setYAxis(new ArrayList<HIYAxis>(){{add(hiyAxis);}});
 
-        HIXrange series1 = new HIXrange();
-        series1.setName("Project 1");
-        series1.setPointPadding(0);
-        series1.setGroupZPadding(0);
-        series1.setBorderColor(HIColor.initWithName("gray"));
-        series1.setPointWidth(20);
+        HITooltip tooltip = new HITooltip();
+        tooltip.setHeaderFormat("<span style=\"font-size:15px\">{point.key}</span><table>");
+        tooltip.setPointFormat("<tr><td style=\"color:{series.color};padding:0\">{series.name}: </td>" + "<td style=\"padding:0\"><b>{point.y}</b></td></tr>");
+        tooltip.setFooterFormat("</talble>");
 
-        HashMap<String, Object> map1 = new HashMap<>();
-        map1.put("x", 1416528000000L);
-        map1.put("x2", 1417478400000L);
-        map1.put("y" , 0);
-        map1.put("partialFill" , 0.25);
+        HIPlotOptions plotOptions = new HIPlotOptions();
+        plotOptions.setColumn(new HIColumn());
+        plotOptions.getColumn().setPointPadding(0.2);
+        plotOptions.getColumn().setBorderWidth(0);
+        options.setPlotOptions(plotOptions);
 
-        HashMap<String, Object> map2 = new HashMap<>();
-        map2.put("x" , 1417478400000L);
-        map2.put("x2" , 1417478400000L);
-        map2.put("y" , 1);
+        HIColumn realMadrid = new HIColumn();
+        realMadrid.setName("Real Madrid");
+        ArrayList<Integer> realMadridData = new ArrayList<>();
+        realMadridData.add(36);
+        realMadridData.add(31);
+        realMadridData.add(93);
+        realMadridData.add(236);
+        realMadrid.setData(realMadridData);
 
-        HashMap<String, Object> map3 = new HashMap<>();
-        map3.put("x" , 1417996800000L);
-        map3.put("x2" , 1418083200000L);
-        map3.put("y" , 2);
+        HIColumn juventus = new HIColumn();
+        juventus.setName("Juventus");
+        ArrayList<Integer> juventusData = new ArrayList<>();
+        juventusData.add(22);
+        juventusData.add(10);
+        juventusData.add(66);
+        juventusData.add(178);
+        juventus.setData(juventusData);
 
-        HashMap<String, Object> map4 = new HashMap<>();
-        map4.put("x" , 1418083200000L);
-        map4.put("x2" , 1418947200000L);
-        map4.put("y" , 1);
+        HIColumn monaco = new HIColumn();
+        monaco.setName("Monaco");
+        ArrayList<Integer> monacoData = new ArrayList<>();
+        monacoData.add(22);
+        monacoData.add(17);
+        monacoData.add(56);
+        monacoData.add(147);
+        monaco.setData(monacoData);
 
-        HashMap<String, Object> map5 = new HashMap<>();
-        map5.put("x" , 1418169600000L);
-        map5.put("x2" , 1419292800000L);
-        map5.put("y" , 2);
+        HIColumn atleticoMadrid = new HIColumn();
+        atleticoMadrid.setName("Atlético Madrid");
+        ArrayList<Integer> atleticoMadridData = new ArrayList<>();
+        atleticoMadridData.add(15);
+        atleticoMadridData.add(9);
+        atleticoMadridData.add(55);
+        atleticoMadridData.add(160);
+        atleticoMadrid.setData(atleticoMadridData);
 
-        series1.setData(new ArrayList<>(Arrays.asList(map1, map2, map3, map4, map5)));
-
-        series1.setDataLabels(new HIDataLabels());
-        series1.getDataLabels().setEnabled(true);
-
-        options.setSeries(new ArrayList<>(Arrays.asList(series1)));
+        ArrayList<HISeries> series = new ArrayList<>();
+        series.add(realMadrid);
+        series.add(juventus);
+        series.add(monaco);
+        series.add(atleticoMadrid);
+        options.setSeries(series);
 
         chartView.setOptions(options);
     }
