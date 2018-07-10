df5f2761ba9cd123e61412b49f8a7544ed43b5aa
bartosz <b.gnacek@gmail.com>
2018-07-10 15:52:02 +0200

    demos: meteogram WIP

--- highcharts-android/devground/src/main/java/com/highsoft/devground/MainActivity.java
+++ highcharts-android/devground/src/main/java/com/highsoft/devground/MainActivity.java
@@ -3,44 +3,38 @@ package com.highsoft.devground;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.highsoft.highcharts.Common.HIChartsClasses.HIAreaspline;
-import com.highsoft.highcharts.Common.HIChartsClasses.HIBackground;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIChart;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIColumn;
-import com.highsoft.highcharts.Common.HIChartsClasses.HIColumnrange;
 import com.highsoft.highcharts.Common.HIChartsClasses.HICredits;
-import com.highsoft.highcharts.Common.HIChartsClasses.HIData;
+import com.highsoft.highcharts.Common.HIChartsClasses.HICrosshair;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIDataLabels;
-import com.highsoft.highcharts.Common.HIChartsClasses.HIGauge;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIHover;
+import com.highsoft.highcharts.Common.HIChartsClasses.HILabel;
 import com.highsoft.highcharts.Common.HIChartsClasses.HILabels;
 import com.highsoft.highcharts.Common.HIChartsClasses.HILegend;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIMarker;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIOptions;
-import com.highsoft.highcharts.Common.HIChartsClasses.HIPane;
-import com.highsoft.highcharts.Common.HIChartsClasses.HIPlotBands;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIPlotLines;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIPlotOptions;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIPosition;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIScrollablePlotArea;
+import com.highsoft.highcharts.Common.HIChartsClasses.HISeries;
 import com.highsoft.highcharts.Common.HIChartsClasses.HISpline;
-import com.highsoft.highcharts.Common.HIChartsClasses.HIStackLabels;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIStates;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIStyle;
-import com.highsoft.highcharts.Common.HIChartsClasses.HISubtitle;
 import com.highsoft.highcharts.Common.HIChartsClasses.HITitle;
 import com.highsoft.highcharts.Common.HIChartsClasses.HITooltip;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIWindbarb;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIXAxis;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIYAxis;
 import com.highsoft.highcharts.Common.HIColor;
-import com.highsoft.highcharts.Common.HIGradient;
-import com.highsoft.highcharts.Common.HIStop;
 import com.highsoft.highcharts.Core.HIChartView;
 import com.highsoft.highcharts.Core.HIFunction;
 
-import java.sql.Date;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -54,65 +48,228 @@ public class MainActivity extends AppCompatActivity {
 
         HIOptions options = new HIOptions();
 
+        HIChart chart = new HIChart();
+        chart.setMarginBottom(70);
+        chart.setMarginRight(40);
+        chart.setMarginTop(50);
+        chart.setPlotBorderWidth(1);
+        chart.setHeight(310);
+        chart.setAlignTicks(false);
+        HIScrollablePlotArea scrollablePlotArea = new HIScrollablePlotArea();
+        scrollablePlotArea.setMinWidth(720);
+        chart.setScrollablePlotArea(scrollablePlotArea);
+
+        options.setChart(chart);
+
+        //defs
+
         HITitle title = new HITitle();
-        title.setText("Average fruit consumption during one week");
+        title.setText(this.getChartTitle());
+        title.setAlign("left");
+        HIStyle titleStyle = new HIStyle();
+        titleStyle.setWhiteSpace("nowrap");
+        titleStyle.setTextOverflow("ellipsis");
+        title.setStyle(titleStyle);
+
         options.setTitle(title);
 
-        HILegend legend = new HILegend();
-        legend.setLayout("vertical");
-        legend.setAlign("left");
-        legend.setVerticalAlign("top");
-        legend.setX(150);
-        legend.setY(150);
-        legend.setFloating(true);
-        legend.setBorderWidth(1);
-        legend.setBackgroundColor(HIColor.initWithHexValue("FFFFFF"));
-        options.setLegend(legend);
+        HICredits credits = new HICredits();
+        credits.setText("Forecast from <a href=\"http://yr.no\">yr.no</a>");
+        credits.setHref(this.xml.querySelector("credit link").getAttribute("url"));
+        HIPosition creditsPosition = new HIPosition();
+        creditsPosition.setX(-40);
+        credits.setPosition(creditsPosition);
 
-        HIXAxis xaxis = new HIXAxis();
-        String[] categories = new String[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-        xaxis.setCategories(new ArrayList<>(Arrays.asList(categories)));
-        HIPlotBands plotband = new HIPlotBands();
-        plotband.setFrom(4.5);
-        plotband.setTo(6.5);
-        plotband.setColor(HIColor.initWithRGBA(68, 170, 213, 2));
-        xaxis.setPlotBands(new ArrayList<>(Arrays.asList(plotband)));
-        options.setXAxis(new ArrayList<HIXAxis>(){{add(xaxis);}});
-
-        HIYAxis yaxis = new HIYAxis();
-        yaxis.setTitle(new HITitle());
-        yaxis.getTitle().setText("Fruit unit");
-        options.setYAxis(new ArrayList<HIYAxis>(){{add(yaxis);}});
+        options.setCredits(credits);
 
         HITooltip tooltip = new HITooltip();
         tooltip.setShared(true);
-        tooltip.setValueSuffix(" units");
+        tooltip.setUseHTML(true);
+        tooltip.setHeaderFormat("<small>{point.x:%A, %b %e, %H:%M} - {point.point.to:%H:%M}</small><br> <b>{point.point.symbolName}</b><br>");
+
         options.setTooltip(tooltip);
 
-        HICredits credits = new HICredits();
-        credits.setEnabled(false);
-        options.setCredits(credits);
+        HIXAxis bottomXAxis = new HIXAxis();
+        bottomXAxis.setType("datetime");
+        bottomXAxis.setTickInterval(2*36e5); // two hours
+        bottomXAxis.setMinTickInterval(36e5); // one hour
+        bottomXAxis.setTickLength(0);
+        bottomXAxis.setGridLineWidth(1);
+        bottomXAxis.setGridLineColor(HIColor.initWithHexValue("F0F0F0"));
+        bottomXAxis.setStartOnTick(false);
+        bottomXAxis.setEndOnTick(false);
+        bottomXAxis.setMinPadding(0);
+        bottomXAxis.setMaxPadding(0);
+        bottomXAxis.setOffset(30);
+        bottomXAxis.setShowLastLabel(true);
+        HILabels bottomXAxisLables = new HILabels();
+        bottomXAxisLables.setFormat("{value:%H}");
+        bottomXAxis.setLabels(bottomXAxisLables);
 
-        HIPlotOptions plotOptions = new HIPlotOptions();
-        plotOptions.setAreaspline(new HIAreaspline());
-        plotOptions.getAreaspline().setFillOpacity(0.5);
-        options.setPlotOptions(plotOptions);
+        HIXAxis topXAxis = new HIXAxis();
+        topXAxis.setLinkedTo(0);
+        topXAxis.setType("datetime");
+        topXAxis.setTickInterval(24*3600*1000);
+        HILabels topXAxisLabels = new HILabels();
+        topXAxisLabels.setFormat("{value:<span style=\"font-size: 12px; font-weight: bold\">%a</span> %b %e}");
+        topXAxisLabels.setAlign("left");
+        topXAxisLabels.setX(3);
+        topXAxisLabels.setY(-5);
+        topXAxis.setLabels(topXAxisLabels);
+        topXAxis.setOpposite(true);
+        topXAxis.setTickLength(20);
+        topXAxis.setGridLineWidth(1);
 
-        HIAreaspline areaspline1 = new HIAreaspline();
-        areaspline1.setName("John");
-        Number[] areaspline1Data = new Number[] {3, 4, 3, 5, 4, 10, 12 };
-        areaspline1.setData(new ArrayList<>(Arrays.asList(areaspline1Data)));
+        options.setXAxis(new ArrayList<>(Arrays.asList(bottomXAxis, topXAxis)));
 
-        HIAreaspline areaspline2 = new HIAreaspline();
-        areaspline2.setName("Jane");
-        Number[] areaspline2Data = new Number[] { 1, 3, 4, 3, 3, 5, 4 };
-        areaspline2.setData(new ArrayList<>(Arrays.asList(areaspline2Data)));
+        HIYAxis temperatureAxis = new HIYAxis();
+        temperatureAxis.setTitle(new HITitle());
+        temperatureAxis.getTitle().setText(null);
+        HILabels temperatureAxisLabels = new HILabels();
+        temperatureAxisLabels.setFormat("{value}°");
+        temperatureAxisLabels.setStyle(new HIStyle());
+        temperatureAxisLabels.getStyle().setFontSize("10px");
+        temperatureAxisLabels.setX(-3);
+        temperatureAxis.setLabels(temperatureAxisLabels);
+        HIPlotLines temperatureAxisPlotLines = new HIPlotLines(); // zero plane
+        temperatureAxisPlotLines.setValue(0);
+        temperatureAxisPlotLines.setColor(HIColor.initWithHexValue("BBBBBB"));
+        temperatureAxisPlotLines.setWidth(1);
+        temperatureAxisPlotLines.setZIndex(2);
+        temperatureAxis.setPlotLines(new ArrayList<>(Collections.singletonList(temperatureAxisPlotLines)));
+        temperatureAxis.setMaxPadding(0.3);
+        temperatureAxis.setMinRange(8);
+        temperatureAxis.setTickInterval(1);
+        temperatureAxis.setGridLineColor(HIColor.initWithHexValue("F0F0F0"));
 
-        options.setSeries(new ArrayList<>(Arrays.asList(areaspline1, areaspline2)));
+        HIYAxis precipitationAxis = new HIYAxis();
+        precipitationAxis.setTitle(new HITitle());
+        precipitationAxis.getTitle().setText(null);
+        precipitationAxis.setLabels(new HILabels());
+        precipitationAxis.getLabels().setEnabled(false);
+        precipitationAxis.setGridLineWidth(0);
+        precipitationAxis.setTickLength(0);
+        precipitationAxis.setMinRange(10);
+        precipitationAxis.setMin(0);
+
+        HIYAxis airPressure = new HIYAxis();
+        airPressure.setAllowDecimals(false);
+        HITitle airPressureTitle = new HITitle();
+        airPressureTitle.setText("hPa");
+        airPressureTitle.setOffset(0);
+        airPressureTitle.setAlign("high");
+        airPressureTitle.setRotation(0);
+        airPressureTitle.setStyle(new HIStyle());
+        airPressureTitle.getStyle().setFontSize("10px");
+        airPressureTitle.getStyle().setColor("#bbc21");
+        airPressureTitle.setAlign("left");
+        airPressureTitle.setX(3);
+        HILabels airPressureLabels = new HILabels();
+        airPressureLabels.setStyle(new HIStyle());
+        airPressureLabels.getStyle().setFontSize("8px");
+        airPressureLabels.getStyle().setColor("bbc21");
+        airPressureLabels.setY(2);
+        airPressureLabels.setX(3);
+        airPressure.setLabels(airPressureLabels);
+        airPressure.setGridLineWidth(0);
+        airPressure.setOpposite(true);
+        airPressure.setShowLastLabel(false);
+
+        options.setYAxis(new ArrayList<>(Arrays.asList(temperatureAxis, precipitationAxis, airPressure)));
+
+        options.setLegend(new HILegend());
+        options.getLegend().setEnabled(false);
+
+        options.setPlotOptions(new HIPlotOptions());
+        options.getPlotOptions().setSeries(new HISeries());
+        options.getPlotOptions().getSeries().setPointPlacement("between");
+
+        options.setSeries(new ArrayList<>());
+
+        HISpline temperatureSeries = new HISpline();
+        temperatureSeries.setName("Temperature");
+        temperatureSeries.setData(this.temperatures);
+        HIMarker temperatureMarker = new HIMarker();
+        temperatureMarker.setEnabled(false);
+        temperatureMarker.setStates(new HIStates());
+        temperatureMarker.getStates().setHover(new HIHover());
+        temperatureMarker.getStates().getHover().setEnabled(true);
+        temperatureSeries.setMarker(temperatureMarker);
+        temperatureSeries.setTooltip(new HITooltip());
+        temperatureSeries.getTooltip().setPointFormat("<span style=\"color:{point.color}\">\u25CF</span> {series.name}: <b>{point.value}°C</b><br/>");
+        temperatureSeries.setZIndex(1);
+        temperatureSeries.setColor(HIColor.initWithHexValue("FF3333"));
+        temperatureSeries.setNegativeColor(HIColor.initWithHexValue("48AFE8"));
+        options.getSeries().add(temperatureSeries);
+
+        HIColumn precipErrSeries = new HIColumn();
+        precipErrSeries.setName("Precipitation");
+        precipErrSeries.setData(this.precipitationsError);
+//        precipErrSeries.setColor(); // 'url(#precipitation-error)' ??!!
+        precipErrSeries.setYAxis(1);
+        precipErrSeries.setGroupPadding(0);
+        precipErrSeries.setPointPadding(0);
+        precipErrSeries.setTooltip(new HITooltip());
+        precipErrSeries.getTooltip().setValueSuffix(" mm");
+        precipErrSeries.getTooltip().setPointFormat("<span style=\"color:{point.color}\">\\u25CF</span> {series.name}: <b>{point.minvalue} mm - {point.maxvalue} mm</b><br/>");
+        precipErrSeries.setGrouping(false);
+        HIDataLabels precipErrDataLabels = new HIDataLabels();
+        precipErrDataLabels.setEnabled(meteogram.hasPrecipitationError);
+        precipErrDataLabels.setFormatter(new HIFunction("function() { if (this.point.maxvalue > 0) { return this.point.maxvalue; }"));
+        precipErrDataLabels.setStyle(new HIStyle());
+        precipErrDataLabels.getStyle().setFontSize("8px");
+        precipErrDataLabels.getStyle().setColor("gray");
+        options.getSeries().add(precipErrSeries);
+
+        HIColumn precipSeries = new HIColumn();
+        precipSeries.setName("Precipitation");
+        precipSeries.setData(this.precipitations);
+        precipSeries.setColor(HIColor.initWithHexValue("68CFE8"));
+        precipSeries.setYAxis(1);
+        precipSeries.setGroupPadding(0);
+        precipSeries.setPointPadding(0);
+        precipSeries.setGrouping(false);
+        HIDataLabels precipDataLabels = new HIDataLabels();
+        precipDataLabels.setEnabled(!meteogram.hasPrecipitationError);
+        precipDataLabels.setFormatter(new HIFunction("function () { if (this.y > 0) { return this.y; }"));
+        precipDataLabels.setStyle(new HIStyle());
+        precipDataLabels.getStyle().setFontSize("8px");
+        precipDataLabels.getStyle().setColor("gray");
+        precipSeries.setTooltip(new HITooltip());
+        precipSeries.getTooltip().setValueSuffix(" mm");
+        options.getSeries().add(precipSeries);
+
+        HISeries airPressureSeries = new HISeries();
+        airPressureSeries.setName("Air pressure");
+        airPressureSeries.setColor(HIColor.initWithHexValue("bbc21"));
+        airPressureSeries.setData(this.pressures);
+        airPressureSeries.setMarker(new HIMarker());
+        airPressureSeries.getMarker().setEnabled(false);
+        airPressureSeries.setShadow(false);
+        airPressureSeries.setTooltip(new HITooltip());
+        airPressureSeries.getTooltip().setValueSuffix(" hPa");
+        airPressureSeries.setDashStyle("shortdot");
+        airPressureSeries.setYAxis(2);
+        options.getSeries().add(airPressureSeries);
+
+        HIWindbarb windSeries = new HIWindbarb();
+        windSeries.setId("windbarbs");
+        windSeries.setColor(HIColor.initWithHexValue("0d233a"));
+        windSeries.setLineWidth(1.5);
+        windSeries.setData(this.winds);
+        windSeries.setVectorLength(18);
+        windSeries.setYOffset(-15);
+        windSeries.setTooltip(new HITooltip());
+        windSeries.getTooltip().setValueSuffix(" m/s");
+        options.getSeries().add(windSeries);
 
         chartView.setOptions(options);
     }
 
+    private String getChartTitle(){
+       return "Meteogram for " + this.xml.querySelector("location name").textContent
+               + ", " + this.xml.querySelector("location country").textContent;
+    }
     /**
      * This method is just for demo purposes.
      * You can pass as an argument your data (e.g. from file)
