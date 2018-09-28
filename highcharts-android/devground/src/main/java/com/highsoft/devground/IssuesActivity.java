37b528bb6483619d6c0bf5be44edfcd85bda34df
bartosz <b.gnacek@gmail.com>
2018-09-28 13:29:18 +0200

    v6.1.4 update

--- highcharts-android/devground/src/main/java/com/highsoft/devground/IssuesActivity.java
+++ highcharts-android/devground/src/main/java/com/highsoft/devground/IssuesActivity.java
@@ -2,16 +2,30 @@ package com.highsoft.devground;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.highsoft.highcharts.Common.HIChartsClasses.HIAxis;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIChart;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIColumn;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIData;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIExporting;
+import com.highsoft.highcharts.Common.HIChartsClasses.HILabels;
+import com.highsoft.highcharts.Common.HIChartsClasses.HILine;
 import com.highsoft.highcharts.Common.HIChartsClasses.HIOptions;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIScrollablePlotArea;
 import com.highsoft.highcharts.Common.HIChartsClasses.HISeries;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIStyle;
+import com.highsoft.highcharts.Common.HIChartsClasses.HITitle;
+import com.highsoft.highcharts.Common.HIChartsClasses.HITooltip;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIXAxis;
+import com.highsoft.highcharts.Common.HIChartsClasses.HIYAxis;
 import com.highsoft.highcharts.Common.HIColor;
 import com.highsoft.highcharts.Core.HIChartView;
+import com.highsoft.highcharts.Core.HIFunction;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class IssuesActivity extends AppCompatActivity {
 
@@ -22,97 +36,83 @@ public class IssuesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_issues);
 
+        Button btn = findViewById(R.id.btn);
+
         HIChartView chartView = findViewById(R.id.hc);
         HIOptions options = new HIOptions();
 
-        options.setExporting(new HIExporting());
-        options.getExporting().setEnabled(false);
+//        HIChart chart = new HIChart();
+//        HIScrollablePlotArea scrollablePlotArea = new HIScrollablePlotArea();
+//        scrollablePlotArea.setMinWidth(1000);
+//        scrollablePlotArea.setScrollPositionX(1);
+//        chart.setScrollablePlotArea(scrollablePlotArea);
+//        options.setChart(chart);
 
-//
-//        Button btn = findViewById(R.id.btn);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                final LayoutInflater inflater = getLayoutInflater();
-//                final View alertLayout = inflater.inflate(R.layout.dialog_custom, null);
-//
-//                HIChartView chartView = alertLayout.findViewById(R.id.hc);
-//                HIColumn column = new HIColumn();
-//                column.setType("column");
-//                column.setName("Rainfall");
-//                ArrayList<HIColor> listColors = new ArrayList<>();
-//                Number[] columnData = new Number[] {49.9, 71.5, 106.4};
-//                HIColor c1 = HIColor.initWithHexValue("ff0000");
-//                HIColor c2 = HIColor.initWithHexValue("00ff00");
-//                HIColor c3 = HIColor.initWithHexValue("0000ff");
-//                listColors.add(c1);
-//                listColors.add(c2);
-//                listColors.add(c3);
-//                options.setColors(listColors);
-//                column.setData(new ArrayList<>(Arrays.asList(columnData)));
-//                column.setTooltip(new HITooltip());
-//                column.getTooltip().setValueSuffix(" mm");
-//
-//                options.setSeries(new ArrayList<>(Collections.singletonList(column)));
-//                chartView.setOptions(options);
-//
-//                AlertDialog.Builder alert = new AlertDialog.Builder(IssuesActivity.this);
-//                alert.setTitle("Graph");
-//                alert.setView(alertLayout);
-//                alert.setCancelable(false);
-//
-//                alert.setNegativeButton("Cancel", (dialog, which) -> Toast.makeText(getBaseContext(), "Cancel clicked", Toast.LENGTH_SHORT).show());
-//                alert.setPositiveButton("Done", (dialog, which) -> Toast.makeText(getBaseContext(), "Done click", Toast.LENGTH_SHORT).show());
-//
-//                AlertDialog dialog = alert.create();
-//                dialog.show();
-//            }
-//        });
+        ArrayList<HIYAxis> yaxis = new ArrayList<>();
+        yaxis.add(new HIYAxis());
+        options.setYAxis(yaxis);
 
+        ArrayList<HIXAxis> xAxes = new ArrayList<>();
+        HIXAxis hixAxis = new HIXAxis();
+        xAxes.add(hixAxis);
+        options.setXAxis(xAxes);
 
-        HIColumn series1 = new HIColumn();
-        series1.setName("Realisasi");
-        ArrayList<ChartData> listData = new ArrayList<>();
-        listData.add(new ChartData(1, "blue"));
-        listData.add(new ChartData(3, "yellow"));
-        listData.add(new ChartData(4, "blue"));
-        listData.add(new ChartData(1, "blue"));
-        ArrayList<HIData> listDataFromApi = new ArrayList<>();
-        for (int i = 0; i < listData.size(); i++) {
-            HIData data = new HIData();
-            data.setY(listData.get(i).realisasi_simplified);
-            if(listData.get(i).color.equals("blue")){
-                HIColor blue = HIColor.initWithHexValue("lol");
-                data.setColor(blue);
+        HITooltip tooltip = new HITooltip();
+        //general formatting
+        tooltip.setBorderWidth(0);
+        tooltip.setShadow(false);
+        tooltip.setBorderColor(HIColor.initWithName("white"));
+        //position of the tooltip
+        tooltip.setPositioner(new HIFunction("function () { return { x: 100, y: 100 }; }"));
+        //styling
+        HIStyle tooltipStyle = new HIStyle();
+        tooltipStyle.setFontFamily("serif");
+        tooltipStyle.setColor("red");
+        tooltip.setStyle(tooltipStyle);
+        //text formatting, two lines
+        tooltip.setFormatter(new HIFunction(f -> {
+            String txt = f.getProperty("series.name") + "<br>" + "Value: " + f.getProperty("x");
+            return txt;
+        }, new String[] {"series.name", "x"}));
+        options.setTooltip(tooltip);
 
-            }else if (listData.get(i).color.equals("yellow")){
-                HIColor yellow = HIColor.initWithHexValue("ffad48");
-                data.setColor(yellow);
-            }
-            listDataFromApi.add(data);
-        }
-        series1.setData(listDataFromApi);
-        series1.setPointPadding(0.2);
-        series1.setPointPlacement(-0.2);
-
-//        HIColumn series3 = new HIColumn();
-//        series3.setName("Target Realisasi");
-//        series3.setColor(HIColor.initWithHexValue("f3f3f3"));
-//        ArrayList<Float> listDataSeries3 = new ArrayList<>();
-//        for (int i = 0; i < listData.size(); i++) {
-//            listDataSeries3.add(listData.get(i).target_simplified);
-//        }
-//        series3.setData(listDataSeries3);
-//        series3.setPointPadding(0.4);
-//        series3.setPointPlacement(-0.2);
-
-        ArrayList<HISeries> listSeries = new ArrayList<>();
-        listSeries.add(series1);
-//        listSeries.add(series3);
-        options.setSeries(listSeries);
+        ArrayList<Number> data1 = new ArrayList<>();
+        data1.add(2);
+        data1.add(1);
+        data1.add(5);
+        data1.add(4);
+        HILine line1 = new HILine();
+        line1.setData(data1);
 
 
+        options.setSeries(new ArrayList<>());
+        options.getSeries().add(line1);
         chartView.setOptions(options);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                HILabels yLablesCompare = new HILabels();
+                HIYAxis yaxis2 = new HIYAxis();
+                yaxis2.setOpposite(true);
+                yaxis2.setLabels(yLablesCompare);
+                options.getYAxis().add(yaxis2);
+
+                ArrayList<Number> data2 = new ArrayList<>();
+                data2.add(4);
+                data2.add(3);
+                data2.add(2);
+                data2.add(6);
+                HILine line2 = new HILine();
+                line2.setYAxis(0);
+                line2.setData(data2);
+                options.getSeries().add(line2);
+
+                chartView.reload();
+            }
+        });
+
 
     }
 }
