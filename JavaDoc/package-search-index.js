e24022f7d6d141a4dd2ee67b5ae8b55f71f7ce0b
Bartosz <b.gnacek@gmail.com>
2022-01-13 17:19:50 +0100

    new javadoc with fixes

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.highsoft.highcharts.common"},{"l":"com.highsoft.highcharts.common.hichartsclasses"},{"l":"com.highsoft.highcharts.core"}]
