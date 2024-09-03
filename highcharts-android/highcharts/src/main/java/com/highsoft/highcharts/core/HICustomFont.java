19684ee8302f0007a7924fe5432ac1898034393e
MikolajMichalczak <michalczakmikolaj@gmail.com>
2024-09-03 12:00:50 +0200

    remove unused code; implement error checking in HICustomFont; implement tests for font adding

--- highcharts-android/highcharts/src/main/java/com/highsoft/highcharts/core/HICustomFont.java
+++ highcharts-android/highcharts/src/main/java/com/highsoft/highcharts/core/HICustomFont.java
@@ -1,6 +1,7 @@
 package com.highsoft.highcharts.core;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.Log;
 import android.util.TypedValue;
 
@@ -8,26 +9,48 @@ class HICustomFont {
 
     private String fontStyle = "";
 
-    //todo errors handling
     public boolean addFont(Context context, int id) {
         String TAG = "Highcharts";
-        if(id == 0){
-            Log.e(TAG, "Font resource can't be found.");
+
+        if (context == null) {
+            Log.e(TAG, "Context is null.");
             return false;
         }
-        else if(!context.getResources().getResourceTypeName(id).equals("font")) {
-            Log.e(TAG, "Resource is not a font type!");
+
+        if (id <= 0) {
+            Log.e(TAG, "Invalid font resource ID.");
             return false;
         }
-        String fontName = context.getResources().getResourceEntryName(id);
-        TypedValue value = new TypedValue();
-        context.getResources().getValue(id, value, true);
-        String path = value.string.toString();
-        String name = path.substring(path.lastIndexOf("/")+1, path.length());
-        String fontFaceTemplate = "<style type=\"text/css\">@font-face { font-family: '%s'; src: url('file:///android_res/font/%s'); } </style>";
-        this.fontStyle = this.fontStyle + String.format(fontFaceTemplate, fontName, name);
-        Log.i(TAG, "FontStyle: " + this.fontStyle);
-        return true;
+
+        try {
+            if (!context.getResources().getResourceTypeName(id).equals("font")) {
+                Log.e(TAG, "Resource is not a font type!");
+                return false;
+            }
+
+            String fontName = context.getResources().getResourceEntryName(id);
+            TypedValue value = new TypedValue();
+            context.getResources().getValue(id, value, true);
+
+            if (value.string == null) {
+                Log.e(TAG, "Font resource path is null.");
+                return false;
+            }
+
+            String path = value.string.toString();
+            String name = path.substring(path.lastIndexOf("/") + 1);
+
+            String fontFaceTemplate = "<style type=\"text/css\">@font-face { font-family: '%s'; src: url('file:///android_res/font/%s'); } </style>";
+            this.fontStyle += String.format(fontFaceTemplate, fontName, name);
+
+            Log.i(TAG, "FontStyle: " + this.fontStyle);
+            return true;
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Font resource not found: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "An unexpected error occurred: " + e.getMessage());
+        }
+        return false;
     }
 
     public String getFont() {
